a082916ed60c63888babcc2f836cca8d12f6aaeb
Loren Kelley <loren.kelley@mhp.com>
2019-11-07 11:15:09 -0500

    creating schema

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 import express from "express";
+import graphlHTTP from "express-graphql";
 import mongoose from "mongoose";
-
+import schema from "./schema";
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb ://localhost/notetaking_db", {
-    userNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect("mongodb://localhost/notetaking_db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
-
 const app = express();
 const PORT = 4300;
-
 app.get("/", (req, res) => {
-    //req = request 
-    // res= response  
   res.json({
     message: "Notetaking API v1"
   });
 });
-
+app.use(
+  "/graphql",
+  graphlHTTP({
+    schema: schema,
+    graphiql: true
+  })
+);
 app.listen(PORT, () => {
   console.log(`Server is listening on PORT ${PORT}`);
 });
---
+++ schema.js
@@ -0,0 +1,25 @@
+import {makeExecutableSchema} from 'Graphql-tools'
+import {resolvers} from  './resolvers';
+
+const typeDefs = `
+type Note {
+    _id: ID!
+    title: String!,
+    date: Date,
+    content: String!
+}
+
+scalar Date
+
+type Query {
+    allNotes: [Note]
+}
+`;
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
+
+export default schema;
+
